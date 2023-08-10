a7cb10e439a8b78b23d07e73866754c5c414978a
Cole Hayden <colehayden@Coles-Mac-mini.local>
2023-08-09 20:50:44 -0500

    init

---
+++ app/books/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <div className='booksText'>
+      <h1>Cole Hayden</h1>
+      <h3>Books I've read</h3>
+      <h5>These are some of my favorite books that I've read and what I think about them.  I'm a lifelong reader and I'll
+        will keep updating this page, so bookmark it if you want to check back in a few months.
+      </h5>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,30 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './styles.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <main>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <h1 className={styles.alignCenter}>
+        Hello!  My name is Cole Hayden.  I'm an avid <a href="/books">bookworm</a>,&nbsp;  
+        <a href="/music">musician</a>, and&nbsp;
+         <a href="/code">software developer</a>. 
+       </h1>
+       
+       <br></br>
+       <br></br>
+       <br></br>
+       <h5 className={styles.alignCenter}>The point of this site is for me to put stuff on it and for you to look at it.</h5>
+       <br></br>
+       <br></br>
+       <br></br>
+       <br></br>
+       <br></br>
+       <h3 className={styles.alignCenter}>Quote of the day</h3>
+       <h3 className={styles.alignCenter}>“You will become way less concerned with what other people think of you when you realize how seldom they do.” 
+― David Foster Wallace, Infinite Jest</h3>
+</div>
     </main>
   )
 }
